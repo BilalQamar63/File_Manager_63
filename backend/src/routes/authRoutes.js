20a24e8c0359f387acff7f3ad553ce2d93355a2c
BilalQamar63 <bilalqamardeen@gmail.com.com>
2025-07-23 20:17:54 +0500

    Backend Url Updated

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 
 // User Routes
+router.get('/', (req, res) => {
+  res.send('Hello World');
+});
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/forgotPassword", authController.forgotPassword);
